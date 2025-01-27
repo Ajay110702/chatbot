1c33d6fc51647e43c1168c2474902fe6a7fd7c9e
Ajay110702 <139728870+Ajay110702@users.noreply.github.com>
2025-01-27 23:57:22 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'
 export default {
   content: [
     "./index.html",
@@ -9,6 +10,6 @@ export default {
   },
   plugins: [
     // eslint-disable-next-line no-undef
-    require('daisyui'),
+    daisyui,
   ],
 }
